f4d496f0bd17ff8ea5181c8d54e4acadb44291aa
Samson Kwizela <kwizelasamson@gmail.com>
2025-06-12 16:50:25 +0200

    modified Timer file

---
+++ src/component/Timer.jsx
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react';
+
+const Timer = ({ duration, onTimeUp }) => {
+  const [timeLeft, setTimeLeft] = useState(duration);
+
+  useEffect(() => {
+    if (timeLeft === 0) {
+      onTimeUp();
+      return;
+    }
+    const timer = setTimeout(() => setTimeLeft(timeLeft - 1), 1000);
+    return () => clearTimeout(timer);
+  }, [timeLeft, onTimeUp]);
+
+  return <div className="text-right">Time Left: {timeLeft}s</div>;
+};
+
+export default Timer;
